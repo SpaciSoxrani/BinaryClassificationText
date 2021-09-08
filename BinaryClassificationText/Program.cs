a638d75b49016d619f3df1423bc7698d69f05a90
SpaciSoxrani <thorloky007@gmail.com>
2021-09-08 08:53:17 +0500

    add new datdset + test data

--- BinaryClassificationText/Program.cs
+++ BinaryClassificationText/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using Microsoft.ML;
 using Microsoft.ML.Data;
 using static Microsoft.ML.DataOperationsCatalog;
@@ -11,9 +13,11 @@ namespace BinaryClassificationText
     class Program
     {
         private static string _appPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
-        private static string _trainDataPath => Path.Combine(_appPath, "..", "..", "..", "Data", "issues_train.tsv");
-
         private static string _dataPath => Path.Combine(_appPath, "..", "..", "..", "Data", "yelp_labelled.txt");
+        private static string _dataPredPath => Path.Combine(_appPath, "..", "..", "..", "Data", "all_wrong.tsv");
+
+        private static string _csvPath => Path.Combine(_appPath, "..", "..", "..", "Data", "data2.tsv");
+        private static string _modelPath => Path.Combine(_appPath, "..", "..", "..", "Models", "model.zip");
 
         private static MLContext _mlContext;
 
@@ -27,15 +31,20 @@ namespace BinaryClassificationText
 
             Evaluate(_mlContext, model, splitDataView.TestSet);
 
-            UseModelWithSingleItem(_mlContext, model);
+            SaveModelAsFile(_mlContext, splitDataView.TrainSet.Schema, model);
+
+            //UseModelWithSingleItem(_mlContext, model);
+
+            UseModelWithData(_mlContext, model);
         }
 
         public static TrainTestData LoadData(MLContext mlContext)
         {
             
-            IDataView dataView = mlContext.Data.LoadFromTextFile<EcologyIssue>(_dataPath, hasHeader: false);
-           
-            TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+            IDataView dataView = mlContext.Data.LoadFromTextFile<EcologyIssue>(_csvPath, hasHeader: false);
+
+
+            TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.4);
            
             return splitDataView;
         }
@@ -49,8 +58,9 @@ namespace BinaryClassificationText
            
             Console.WriteLine("=============== Create and Train the Model ===============");
             var model = estimator.Fit(splitTrainSet);
+
             Console.WriteLine("=============== End of training ===============");
-           
+
             return model;
         }
 
@@ -68,27 +78,82 @@ namespace BinaryClassificationText
             Console.WriteLine($"Auc: {metrics.AreaUnderRocCurve:P2}");
             Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
             Console.WriteLine("=============== End of model evaluation ===============");
+
+           
         }
 
-        private static void UseModelWithSingleItem(MLContext mlContext, ITransformer model)
+        //private static void UseModelWithSingleItem(MLContext mlContext, ITransformer model)
+        //{
+        //    var predictionFunction = mlContext.Model.CreatePredictionEngine<EcologyIssue, IssuePrediction>(model);
+
+        //    var flag = true;
+
+        //    while (flag)
+        //    {
+        //        Console.WriteLine("введите текст для проверки");
+
+        //        var newSample = Console.ReadLine();
+        //        EcologyIssue sampleStatement = new EcologyIssue
+        //        {
+        //            SentimentText = newSample
+        //        };
+
+        //        var resultPrediction = predictionFunction.Predict(sampleStatement);
+
+        //        Console.WriteLine();
+        //        Console.WriteLine("=============== Prediction Test of model with a single sample and test dataset ===============");
+
+        //        Console.WriteLine();
+        //        Console.WriteLine($"Prediction: {(Convert.ToBoolean(resultPrediction.Prediction) ? "Positive" : "Negative")} | Probability: {resultPrediction.Probability} ");
+
+        //        Console.WriteLine("=============== End of Predictions ===============");
+        //        Console.WriteLine();
+        //    }
+        //}
+
+        private static void UseModelWithData(MLContext mlContext, ITransformer model)
         {
-            var predictionFunction = mlContext.Model.CreatePredictionEngine<EcologyIssue, IssuePrediction>(model);
+            //загрузка данных из проверочного файла
+            IDataView dataNewPredView = mlContext.Data.LoadFromTextFile<EcologyIssue>(_dataPredPath, hasHeader: false);
+            //IDataView predictions = model.Transform(dataNewPredView);
 
-            EcologyIssue sampleStatement = new EcologyIssue
+            //для оценки каждого текста
+            //var predictionFunction = mlContext.Model.CreatePredictionEngine<EcologyIssue, IssuePrediction>(model);
+
+            IDataView predictions = model.Transform(dataNewPredView);
+            var predictedResults = mlContext.Data.CreateEnumerable<IssuePrediction>(predictions, reuseRowObject: false);
+
+            Console.WriteLine();
+
+            Console.WriteLine("=============== Prediction Test of loaded model with multiple samples ===============");
+
+            Console.WriteLine();
+
+            foreach (IssuePrediction prediction in predictedResults)
             {
-                SentimentText = "Одним из первых Жениных проектов было обновление БД и бэкенда для ленты новостей. Другим — хранение и доставка уведомлений о важных событиях в шапке сайта, архитектуру которого он написал с нуля. Благодаря усилиям Жени все картинки в VK хранятся максимально компактно — это самый большой проект, которым он занимался. В прошлом году он возглавил команду, в которую когда-то пришел, и стал отличным руководителем, — передаёт слова коллег ребёнка E1.RU."
-            };
-            
-            var resultPrediction = predictionFunction.Predict(sampleStatement);
-            
-            Console.WriteLine();
-            Console.WriteLine("=============== Prediction Test of model with a single sample and test dataset ===============");
+                Console.WriteLine($"Prediction: {(Convert.ToBoolean(prediction.Prediction) ? "Positive" : "Negative")} | Probability: {prediction.Probability} ");
+            }
+            Console.WriteLine("=============== End of predictions ===============");
 
-            Console.WriteLine();
-            Console.WriteLine($"Sentiment: {resultPrediction.SentimentText} | Prediction: {(Convert.ToBoolean(resultPrediction.Prediction) ? "Positive" : "Negative")} | Probability: {resultPrediction.Probability} ");
 
-            Console.WriteLine("=============== End of Predictions ===============");
-            Console.WriteLine();
+
+
+            //StreamReader sr = new StreamReader(_dataNewPredPath);
+
+            //List<EcologyIssue> sentiments = new List<EcologyIssue> { };
+            //foreach (EcologyIssue prediction in predictedResults)
+            //{
+            //    var line = sr.ReadLine();
+            //    var sent = new EcologyIssue { SentimentText = line };
+            //    var res = predictionFunction.Predict(sent);
+            //    Console.WriteLine($"Sentiment: {res.SentimentText} | Prediction: {(Convert.ToBoolean(res.Prediction) ? "Positive" : "Negative")} | Probability: {res.Probability} ");
+            //}
+        }
+        private static void SaveModelAsFile(MLContext mlContext, DataViewSchema trainingDataViewSchema, ITransformer model)
+        {
+            mlContext.Model.Save(model, trainingDataViewSchema, _modelPath);
+          
+            Console.WriteLine("The model is saved to {0}", _modelPath);
         }
     }
 }
